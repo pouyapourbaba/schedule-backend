bbea8df596dd9576b28c6187b774a9d2d2270ece
Pouya Pourbaba <p.pourbaba@gmail.com>
2019-04-11 19:55:50 +0300

    DB logger, unhandled excep, rejected promises

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-require("express-async-errors")
-const winston = require("winston")
-const morgan = require("morgan")
-const helmet = require("helmet")
+require("express-async-errors");
+const winston = require("winston");
+require("winston-mongodb");
+const morgan = require("morgan");
+const helmet = require("helmet");
 const cors = require("cors");
 const config = require("config");
 const express = require("express");
 const mongoose = require("mongoose");
-const error = require("./middleware/error")
+const error = require("./middleware/error");
 const users = require("./routes/users");
 const login = require("./routes/login");
 const todos = require("./routes/todos");
 const tasks = require("./routes/tasks");
 const app = express();
 
-winston.add(new winston.transports.File({ filename: "logfile.log"}))
+// handle uncaught exceptions and exit the process
+winston.exceptions.handle(
+  new winston.transports.File({ filename: "uncaughtExceptions.log" })
+);
+
+// handle unhandled rejections and exit the process
+process.on("unhandledRejection", ex => {
+  throw ex;
+});
+
+// log the errors into a file and the db
+winston.add(new winston.transports.File({ filename: "logfile.log" }));
+winston.add(
+  new winston.transports.MongoDB({
+    db: "mongodb://localhost/scheduler",
+    level: "warn"
+  })
+);
 
 // check the jwtPrivateKey in the environment variables
 if (!config.get("jwtPrivateKey")) {
@@ -32,8 +50,8 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(helmet())
-app.use(morgan("dev"))
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.use("/api/users", users);
 app.use("/api/login", login);
@@ -41,7 +59,7 @@ app.use("/api/todos", todos);
 app.use("/api/tasks", tasks);
 
 // error handler middleware
-app.use(error)
+app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
