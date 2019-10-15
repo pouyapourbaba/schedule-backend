9a13649ec58cec9c0586acfad10cf6a32487d37e
Pouya Pourbaba <p.pourbaba@gmail.com>
2019-10-15 22:21:55 +0300

    convert user provided email to lowercase when login and register

--- routes/auth.js
+++ routes/auth.js
@@ -1,43 +1,44 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const bcrypt = require("bcrypt");
-const { User, userAuthenticationValidator } = require("../models/user");
-const { validationResult } = require("express-validator");
-const auth = require("../middleware/auth");
+const bcrypt = require('bcrypt');
+const { User, userAuthenticationValidator } = require('../models/user');
+const { validationResult } = require('express-validator');
+const auth = require('../middleware/auth');
 
 // @route   GET api/auth
 // @desc    Test user
 // @access  Private
-router.get("/", auth, async (req, res) => {
-  const user = await User.findById(req.user.id).select("-password");
+router.get('/', auth, async (req, res) => {
+  const user = await User.findById(req.user.id).select('-password');
   res.send(user);
 });
 
 // @route   POST api/auth
 // @desc    Authenticate user & get token
 // @access  Public
-router.post("/", userAuthenticationValidator, async (req, res) => {
+router.post('/', userAuthenticationValidator, async (req, res) => {
   // validate the user by Joi and JOI
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  const { email, password } = req.body;
-
+  let { email, password } = req.body;
+  email = email.toLowerCase();
+  
   // check to see if the user with the entered email already exists
-  let user = await User.findOne({ email });
+  let user = await User.findOne({ email: email.toLowerCase() });
   if (!user)
     return res
       .status(400)
-      .json({ errors: [{ msg: "Invalid email or password" }] });
+      .json({ errors: [{ msg: 'Invalid email or password' }] });
 
   // compare the hashed password with the entered password
   const validPassword = await bcrypt.compare(password, user.password);
   if (!validPassword)
     return res
       .status(400)
-      .json({ errors: [{ msg: "Invalid email or password" }] });
+      .json({ errors: [{ msg: 'Invalid email or password' }] });
 
   // create a jwt when the user logs in and set it in the response header
   const token = user.generateAuthToken();
